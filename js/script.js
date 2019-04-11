2e74e7d1b39896a9a1f412b4f862608b2212540f
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2019-04-11 23:49:09 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// document.write('Hello world!');
+// alert('hello again!');
+
+//javascript ignores the whitespaces
+
+//lesson7
+/* document.write("Hello World");
+document.write("Hello World");
+document.write("Hello World");
+document.write("Hello World"); */
+
+//document.write("GoodBye"); //change to GoodBye!s
